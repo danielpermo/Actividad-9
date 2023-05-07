606ff5a9d4afa756a365f53881a388e7f6538e46
Daniel Perez <danielpermo@gmail.com>
2023-05-07 13:17:43 +0200

    Añadir a la query de posts_autor los campos nombre, email e imagen del autor

--- appBlogActividad9DanielPerez/models/post_autor.model.js
+++ appBlogActividad9DanielPerez/models/post_autor.model.js
@@ -1,5 +1,5 @@
 const getByAutorId = (autorId) => {
-    return db.query('select * from posts where autor_idautor = ?', [autorId]);
+    return db.query('select posts.*, a.nombre, a.email, a.imagen from posts, autores as a where autor_idautor = ? and autor_idautor = idautor', [autorId]);
 }
 
 
